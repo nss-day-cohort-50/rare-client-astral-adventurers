acdb8f0748d1dff5e6172f2d1c81ec507b529b9c
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-11-17 08:35:46 -0600

    merge

--- src/components/posts/PostManager.js
+++ src/components/posts/PostManager.js
@@ -1,15 +1,15 @@
 export const getAllPosts = () => {
-    return fetch("http://localhost:8088/posts")
+    return fetch("http://localhost:8000/posts")
     .then(res => res.json())
 };
 
 export const getPostById = (id) => {
-    return fetch(`http://localhost:8088/posts/${id}`)
+    return fetch(`http://localhost:8000/posts/${id}`)
     .then(res => res.json())
 };
 
 export const deletePost = (postId, func) => {
-    fetch(`http://localhost:8088/posts/${postId}`, {
+    fetch(`http://localhost:8000/posts/${postId}`, {
         method: "DELETE"
     })
         .then(func)
--- src/components/posts/UserPostList.js
+++ src/components/posts/UserPostList.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react"
-import { useHistory, Link } from "react-router-dom"
+// import { useHistory, Link } from "react-router-dom"
 import { getAllPosts } from "./PostManager";
-import { FaEdit } from "react-icons/fa"
+// import { FaEdit } from "react-icons/fa"
 
 export const UserPostList = (props) => {
     console.log(props)
-    const history = useHistory()
+    // const history = useHistory()
     const [posts, setPosts] = useState([])
 
 
@@ -20,37 +20,32 @@ export const UserPostList = (props) => {
 
             <h2 className="title">Your Posts</h2>
             <div className="allPosts">
-            <i class="fas fa-edit"></i>
+
                 {
-                    posts.map((post) => {
+                    posts?.map((post) => {
                       
                         if (post?.user_id === parseInt(localStorage.getItem("rare_user_id"))) {
-                            
-                            return <>
-                                
+                        
+                         <>
                                 <div className="space-between">
                                     <h4 className="mp-title">Title: {post.title}</h4>
                                     <p>Author: {post.user.first_name} {post.user.last_name}</p>
                                     <p>Date: {post.publication_date}</p>
                                     <p>{post.content}</p>
                                     <p>Category: {post.category.label}</p>
-                                    
-                                    
-
+                                
                                     <div className="buttons">
-
+                                    {post.published} ?
+                                    <button>Unpublish</button> :
+                                    <button>Publish</button>
                                     </div>
                                 </div>
-                            </>
+                          </>
 
 
                         }
                     }
-                    )
-                }
-
-                {/* <button className="btn btn-secondary" onClick={() => history.push("/")}>
-                    HOME</button> */}
+                    )}
             </div>
 
         </>
