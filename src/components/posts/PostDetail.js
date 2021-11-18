ba129d89871fea9033f746b8ee2a701a7878953e
Erin <etruman2@gmail.com>
2021-11-18 12:12:39 -0600

    added ternary to image, added link to user post, made image show up

--- src/components/posts/PostDetail.js
+++ src/components/posts/PostDetail.js
@@ -5,27 +5,52 @@ import { getPostById } from "./PostManager"  // <---.js or no?
 //import author post detail view css 
 
 export const PostDetails = () => {
-    const [post, setPost] = useState({}) 
+    const [post, setPost] = useState({})
     const { postId } = useParams()
     //const history = useHistory()  <--- for future edit post button
-    
+
     useEffect(() => {
-        getPostById(postId)
-            .then(setPost)
-    }, [])
+        getPostById(postId).then((res) => {
+    setPost({
+        id: res.id,
+        title: res.title,
+        imageUrl: res.image_url,
+        publicationDate: res.publication_date,
+        content: res.content,
+        username: res.author.user.username,
+        label: res.category.label
+    })
+})
+    },
+[])
 
-    return (
-        <section className="post">
-            <h3 className="post__title">{post.title}</h3>
-            <div className="post__publication_date">{post.publication_date}</div>
-            <div className="post__content">{post.content}</div>
-            <div className="post__user_id">By {post.user?.first_name}</div>    
-            <div className="post__category_name"> {post.category?.label}</div>
+return (
+    <section className="post">
+        <h3 className="post__title">Title: {post.title}</h3>
+        <div className="post__publication_date">{post.imageUrl!= null ? <img src={post.imageUrl} alt="profile Picture" />: ""}</div>
+        <div className="post__publication_date">Date Published: {post.publicationDate}</div>
+        <div className="post__content">Content: {post.content}</div>
+        <div className="post__user_id">By: {post.username}</div>
+        <div className="post__category_name">Category: {post.label}</div>
 
-            {/* <button onClick={() => {                            <--- for future edit post button
+        {/* <button onClick={() => {                            <--- for future edit post button
                 history.push(`/posts/edit/${post.id}`)
             }}>Edit</button> */}
-       
-        </section>
-    )
+
+    </section>
+)
 }
+
+
+
+
+
+// approved: false
+// author: {id: 2, bio: "hey gurl",…}
+// category: {id: 1, label: "Life"}
+// content: "Ladidaaaaaa"
+// id: 1
+// image_url: null
+// is_published: false
+// publication_date: "2021-11-15"
+// title: "Life"
